7e722e28713f95f1a0f524baa64bcfe31f5bf01b
RonnieTz <ronis.tzol@gmail.com>
2024-06-16 11:29:29 +0100

    fix triangle and labels positions

--- app/components/Triangle.tsx
+++ app/components/Triangle.tsx
@@ -2,28 +2,35 @@ const Triangle = () => {
   return (
     <div
       style={{
-        height: '300px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        position: 'relative',
       }}
     >
-      <div className="triangle_text a">a</div>
-      <div className="triangle_text b">b</div>
-      <div className="triangle_text c">c</div>
-      <div className="triangle_text A">A</div>
-      <div className="triangle_text B">B</div>
-      <div className="triangle_text C">C</div>
+      <div
+        style={{
+          height: '320px',
+          width: '320px',
 
-      <svg width={320} height={320}>
-        <polygon
-          points="90,20 20,300 300,300"
-          fill="white"
-          stroke="black"
-          strokeWidth={5}
-        />
-      </svg>
+          position: 'relative',
+        }}
+      >
+        <div className="triangle_text a">a</div>
+        <div className="triangle_text b">b</div>
+        <div className="triangle_text c">c</div>
+        <div className="triangle_text A">A</div>
+        <div className="triangle_text B">B</div>
+        <div className="triangle_text C">C</div>
+
+        <svg width={320} height={320}>
+          <polygon
+            points="90,20 20,300 300,300"
+            fill="white"
+            stroke="black"
+            strokeWidth={5}
+          />
+        </svg>
+      </div>
     </div>
   );
 };
